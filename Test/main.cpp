caf9a70083b1ee2d3f643b38b2276170d63c309d
王一 <wangyi@dhcp39.sewn2.iit.edu>
2014-10-06 13:50:59 -0500

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by 王一 on 10/6/14.
+//  Copyright (c) 2014 王一. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
